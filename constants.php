71c03623aedf1760d73dcbbf098e7cd73184d69f
Nathan Jeannot <nathan.jeannot@epitech.eu>
2024-09-10 17:44:08 +0200

    fix(constants & loader): remove the require in the constants & fix files name in the loader

--- constants.php
+++ constants.php
@@ -6,9 +6,5 @@
  * @package Skouerr_CLI
  */
 
-if ( class_exists( 'WP_CLI' ) ) {
-	require __DIR__ . '/includes/class-sk-cli-input.php';
-}
-
 const SKOUERR_REMOTE_DOMAIN = 'https://download.skouerr.dev';
 const SKOUERR_REMOTE_THEME_URL = SKOUERR_REMOTE_DOMAIN . '/wp-json/skouerr/v1/download/theme';
--- loader.php
+++ loader.php
@@ -17,17 +17,17 @@ require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/includes/class-skouerr-cli-plugin.php';
 require __DIR__ . '/includes/class-skouerr-cli.php';
 
-require __DIR__ . '/includes/commands/class-sk-cli-import-block.php';
-require __DIR__ . '/includes/commands/class-sk-cli-list-blocks.php';
-require __DIR__ . '/includes/commands/class-sk-cli-make-theme.php';
-require __DIR__ . '/includes/commands/class-sk-cli-make-block.php';
-require __DIR__ . '/includes/commands/class-sk-cli-make-post-type.php';
-require __DIR__ . '/includes/commands/class-sk-cli-make-template.php';
-require __DIR__ . '/includes/commands/class-sk-cli-save-template.php';
-require __DIR__ . '/includes/commands/class-sk-cli-save-pattern.php';
+require __DIR__ . '/includes/commands/class-skouerr-cli-import-block.php';
+require __DIR__ . '/includes/commands/class-skouerr-cli-list-blocks.php';
+require __DIR__ . '/includes/commands/class-skouerr-cli-make-theme.php';
+require __DIR__ . '/includes/commands/class-skouerr-cli-make-block.php';
+require __DIR__ . '/includes/commands/class-skouerr-cli-make-post-type.php';
+require __DIR__ . '/includes/commands/class-skouerr-cli-make-template.php';
+require __DIR__ . '/includes/commands/class-skouerr-cli-save-template.php';
+require __DIR__ . '/includes/commands/class-skouerr-cli-save-pattern.php';
 
 
-require __DIR__ . '/includes/templates/class-sk-template-block.php';
-require __DIR__ . '/includes/templates/class-sk-template-block-native-php.php';
-require __DIR__ . '/includes/templates/class-sk-template-block-acf-php.php';
-require __DIR__ . '/includes/templates/class-sk-template-block-acf-twig.php';
+require __DIR__ . '/includes/templates/class-skouerr-template-block.php';
+require __DIR__ . '/includes/templates/class-skouerr-template-block-native-php.php';
+require __DIR__ . '/includes/templates/class-skouerr-template-block-acf-php.php';
+require __DIR__ . '/includes/templates/class-skouerr-template-block-acf-twig.php';
